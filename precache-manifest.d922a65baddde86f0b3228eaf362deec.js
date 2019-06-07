b70a57369708a92ab9ea89a23022e5b630bb2fd7
mohamed shire <m.x.shire876@hotmail.com>
2019-06-06 20:30:17 -0500

    Updates

---
+++ precache-manifest.d922a65baddde86f0b3228eaf362deec.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cabbb1a2d79d9fdda2a12a229f26f216",
+    "url": "/moshire/react-clcky-game/index.html"
+  },
+  {
+    "revision": "e70bf896cef414ef0726",
+    "url": "/moshire/react-clcky-game/static/css/main.23cb76bd.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/moshire/react-clcky-game/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "e70bf896cef414ef0726",
+    "url": "/moshire/react-clcky-game/static/js/main.7d71269c.chunk.js"
+  },
+  {
+    "revision": "75ba39bfc3fb7f0446f8",
+    "url": "/moshire/react-clcky-game/static/js/runtime~main.85f40629.js"
+  },
+  {
+    "revision": "5d65332cf7c3974591a8283a3f7b3bc8",
+    "url": "/moshire/react-clcky-game/static/media/angelfish.5d65332c.jpg"
+  },
+  {
+    "revision": "52f65e451636729b18a6761c0bc172dd",
+    "url": "/moshire/react-clcky-game/static/media/barracuda.52f65e45.jpeg"
+  },
+  {
+    "revision": "321b172f690c954a5d057d9cb19abb40",
+    "url": "/moshire/react-clcky-game/static/media/butterfly.321b172f.jpg"
+  },
+  {
+    "revision": "df16d5fde20aefd05a35dee5e782b6e7",
+    "url": "/moshire/react-clcky-game/static/media/chromis.df16d5fd.jpg"
+  },
+  {
+    "revision": "1069ab6f31668351a94985d206797f6f",
+    "url": "/moshire/react-clcky-game/static/media/clownfish.1069ab6f.jpg"
+  },
+  {
+    "revision": "49b9dc5fc0dbf35c2a125d2532adaa4e",
+    "url": "/moshire/react-clcky-game/static/media/creolewrasse.49b9dc5f.jpg"
+  },
+  {
+    "revision": "228f9d1463cbbe3b5e0b57908d7ec93c",
+    "url": "/moshire/react-clcky-game/static/media/damselfish.228f9d14.jpg"
+  },
+  {
+    "revision": "06b9c3169370894762d49745d40de4eb",
+    "url": "/moshire/react-clcky-game/static/media/grunt.06b9c316.jpg"
+  },
+  {
+    "revision": "b27ddfcfde7efbf2c4d67b3badcfda94",
+    "url": "/moshire/react-clcky-game/static/media/parrotfish.b27ddfcf.jpg"
+  },
+  {
+    "revision": "b376e509de7469b2f0d252f40a1875cb",
+    "url": "/moshire/react-clcky-game/static/media/rockbeauty.b376e509.jpg"
+  },
+  {
+    "revision": "4395feb1de54b8ed0e358013f32c9b1f",
+    "url": "/moshire/react-clcky-game/static/media/sergeantmajor.4395feb1.JPG"
+  },
+  {
+    "revision": "5247794b72eedcac58a115add1c81d9a",
+    "url": "/moshire/react-clcky-game/static/media/tang.5247794b.jpg"
+  }
+]);
